3aff0067832d94fff5fa94ab4c3ddd9a5d35e0d3
Vishal Yadav <43027336+Vishal1297@users.noreply.github.com>
2025-05-10 21:44:30 +0530

    Update user-data.sh

--- ec2/user-data.sh
+++ ec2/user-data.sh
@@ -5,7 +5,4 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-echo "current user $(whoami)"
-sudo su
 echo "<h2>Web server running at $(hostname -f)</h2>" > /var/www/html/index.html
-echo "current user $(whoami)"
