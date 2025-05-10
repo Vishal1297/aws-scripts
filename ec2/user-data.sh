45de89a184a9db7fac96de0ce20ae02f24842901
Vishal Yadav <43027336+Vishal1297@users.noreply.github.com>
2025-05-10 21:37:57 +0530

    Create user-data.sh

---
+++ ec2/user-data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# install httpd (Linux 2 version)
+echo "Installing httpd"
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "current user $(whoami)"
+sudo su
+echo "<h2>Web server running at $(hostname -f)</h2>" > /var/www/html/index.html
+exit
+echo "current user $(whoami)"
